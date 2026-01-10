1e33d297ff970ff9f5357c508d44fd022cea0af5
joseanttonnii <joseanttonnii@gmail.com>
2026-01-10 08:39:27 -0300

    novo

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>AlexandreCred</title>
+      <meta name="description" content="Simulação rápida de empréstimo no cartão" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
