3f66ec07f7f31a5431814d25fca11d59044485de
joseanttonnii <joseanttonnii@gmail.com>
2026-01-10 11:04:03 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,13 @@ const formatar = (v: number) =>
 // Taxas de empréstimo
 const taxas = {
   tipo1: {
+    liberado: { 0:4,1:7.5,2:8.25,3:8.75,4:9.5,5:9.99,6:10.75,7:11.25,8:11.75,9:11.95,10:11.99,11:13.75,12:13.99,13:16.5,14:17,15:17.49,16:18,17:19,18:19.99,19:22,20:24,21:24.99 },
+    limite: { 0:3.85,1:6.98,2:7.62,3:8.05,4:8.68,5:9.08,6:9.71,7:10.11,8:10.45,9:10.67,10:10.71,11:12.09,12:12.27,13:14.16,14:14.53,15:14.89,16:15.25,17:15.97,18:16.66,19:18.03,20:19.35,21:19.99 }
+  },
+tipo2: {
     liberado: { 0:5,1:8,2:9,3:10,4:10.5,5:11.5,6:12.5,7:13,8:14,9:14.5,10:14.99,11:15.75,12:15.99,13:18,14:19,15:20.5,16:20.99,17:21.99,18:22.99,19:24.99,20:26.99,21:28.99 },
     limite: { 0:4.76,1:7.41,2:8.26,3:9.09,4:9.5,5:10.31,6:11.11,7:11.5,8:12.28,9:12.66,10:13.04,11:13.61,12:13.78,13:15.25,14:15.97,15:17.01,16:17.36,17:18.03,18:18.7,19:19.99,20:21.26,21:22.47 }
   },
-  tipo2: {
-    liberado: { 0:4,1:7.5,2:8.25,3:8.75,4:9.5,5:9.99,6:10.75,7:11.25,8:11.75,9:11.95,10:11.99,11:13.75,12:13.99,13:16.5,14:17,15:17.49,16:18,17:19,18:19.99,19:22,20:24,21:24.99 },
-    limite: { 0:3.85,1:6.98,2:7.62,3:8.05,4:8.68,5:9.08,6:9.71,7:10.11,8:10.45,9:10.67,10:10.71,11:12.09,12:12.27,13:14.16,14:14.53,15:14.89,16:15.25,17:15.97,18:16.66,19:18.03,20:19.35,21:19.99 }
-  }
 } as const;
 
 type Tipo = keyof typeof taxas;
@@ -57,10 +57,10 @@ export default function Calculadora() {
   const handleCopiar = () => {
     if (!res) return;
 
-    const cartao = tipo === "tipo1" ? "ELO" : "VISA/Master";
+    const cartao = tipo === "tipo1" ? "VISA/MASTER" : "ELO/HIPER";
 
     const texto = `
-=====HiperCred=====
+=====Simulador HiperCred=====
 
 Cartão: ${cartao}
 Valor Liberado: R$ ${formatar(res.valorLiberado)}
@@ -79,12 +79,12 @@ Total a pagar: R$ ${formatar(res.totalPagar)}
 
         {/* Entrada */}
         <div className="bg-blue-900/70 backdrop-blur rounded-2xl p-6 space-y-4 shadow-xl">
-          <h1 className="text-2xl font-bold text-orange-400">Calculadora HiperCred</h1>
-          <p className="text-sm text-blue-200">Simulação de crédito</p>
+          <h1 className="text-2xl font-bold text-orange-400">Simulador HiperCred</h1>
+          <p className="text-sm text-blue-200">Simulação</p>
 
           <select className="w-full p-2 rounded bg-blue-800" value={tipo} onChange={e => setTipo(e.target.value as Tipo)}>
-            <option value="tipo1">ELO</option>
-            <option value="tipo2">VISA / Master</option>
+            <option value="tipo1">VISA/MASTER</option>
+            <option value="tipo2">ELO/HIPER</option>
           </select>
 
           <select className="w-full p-2 rounded bg-blue-800" value={opcao} onChange={e => setOpcao(e.target.value as Opcao)}>
