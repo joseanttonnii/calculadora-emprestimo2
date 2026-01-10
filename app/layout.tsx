d822946cf9489104a9f0d5112bc86d8f86226b06
joseanttonnii <joseanttonnii@gmail.com>
2026-01-10 08:52:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AlexandreCred",
+  title: "HiperCred",
   description: "Simulação rápida de empréstimo no cartão",
 };
 
